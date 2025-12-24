b13b1aa66d6879672624b1047352ed0a9097ffc4
mocadayoo <192976698+mocadayoo@users.noreply.github.com>
2025-12-24 11:59:06 +0900

    make more funny secrets (pedro, snow)

--- js/funny.js
+++ js/funny.js
@@ -1,27 +1,91 @@
-document.addEventListener('visibilitychange', () => {
-    if (document.visibilityState === 'hidden') {
-        document.title = "寂しい..."
+document.addEventListener("visibilitychange", () => {
+    if (document.visibilityState === "hidden") {
+        document.title = "寂しい...";
     } else {
-        document.title = "もかだお！"
+        document.title = "もかだお！";
     }
 });
 
+let pedrobuttonshown = false;
 let rotation = 0;
 const friction = 0.025;
 let speed = 0;
-const iconel = document.getElementById('icon');
-iconel.addEventListener('click', () => {
+let minspeed = 0; // pedroがオンのときは8(曲とちょうどいい)
+const pedroButtonel = document.getElementById("pedro_all");
+const pedroButton = document.getElementById("switch_pedro");
+const pedroaudio = document.getElementById("pedro_audio");
+const iconel = document.getElementById("icon");
+iconel.addEventListener("click", () => {
     speed += 1;
-})
+});
+
+async function watchPedro() {
+    let istoggled = false;
+    pedroButton.addEventListener("click", () => {
+        if (!istoggled) {
+            istoggled = true;
+            minspeed = 8;
+            pedroaudio.volume = 0.4;
+            pedroaudio.play();
+        } else {
+            istoggled = false;
+            minspeed = 0;
+            pedroaudio.pause();
+            pedroaudio.currentTime = 0;
+        }
+    });
+}
+const currentMonth = new Date().getMonth();
+if (currentMonth == 12 - 1) {
+    particlesJS("particles-js", {
+        "particles": {
+            "number": {
+                "value": 200,
+                "density": { "enable": true, "value_area": 800 },
+            },
+            "color": { "value": ["#eaeff3", "#99ccff", "#cccccc"] },
+            "shape": { "type": "circle" },
+            "opacity": {
+                "value": 0.8,
+                "random": true,
+                "anim": { "enable": false },
+            },
+            "size": { "value": 4, "random": true, "anim": { "enable": false } },
+            "line_linked": { "enable": false },
+            "move": {
+                "enable": true,
+                "speed": 2,
+                "direction": "bottom",
+                "random": true,
+                "straight": false,
+                "out_mode": "out",
+                "bounce": false,
+            },
+        },
+        "interactivity": {
+            "detect_on": "canvas",
+            "events": { "onhover": { "enable": false } },
+        },
+        "retina_detect": true,
+    });
+}
 
 function rotate() {
     rotation += speed;
-    
-    if (speed > 0) {
+
+    if (speed > minspeed) {
         speed -= friction;
     }
-    
-    iconel.style.transform = `rotate(${rotation}deg) scale(${1+(speed*0.05)})`;
+
+    if (!pedrobuttonshown && speed > 10) {
+        pedrobuttonshown = true;
+        pedroButtonel.style.display = "block";
+        watchPedro();
+    }
+
+    iconel.style.transform = `rotate(${rotation}deg) scale(${
+        1 + (speed * 0.05)
+    })`;
     requestAnimationFrame(rotate);
 }
 
